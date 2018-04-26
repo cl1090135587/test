e8b3028f74ba67829d991d2fe8c0e172832acd5b
cl1090135587 <cl1090135587@163.coom>
2018-04-26 11:01:00 +0800

    Update App.java

--- src/cc/disruptor/test/App.java
+++ src/cc/disruptor/test/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println(2 + 3);
         System.out.println("out");
+        System.out.println("out2");
     }
 }
