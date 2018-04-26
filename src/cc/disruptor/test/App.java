5fb305e7787e5282ca60a62d39979c7677316b44
chenlong <chenlong@smartpay.com.hk>
2018-04-26 11:37:03 +0800

    test 2

--- src/cc/disruptor/test/App.java
+++ src/cc/disruptor/test/App.java
@@ -4,5 +4,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println(2 + 3);
         System.out.println("out");
+        System.out.println("add");
+        System.out.println("add2");
+//        System.out.println("add3");
+        System.out.println("add4");
     }
 }
