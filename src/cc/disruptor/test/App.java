c77cebd7fd33fecb7a688e21d57e377bfac6b316
chenlong <chenlong@smartpay.com.hk>
2018-04-26 09:31:40 +0800

    Initial commit

---
+++ src/cc/disruptor/test/App.java
@@ -0,0 +1,7 @@
+package cc.disruptor.test;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(2 + 3);
+    }
+}
---
+++ src/cc/disruptor/test/Test.java
@@ -0,0 +1,11 @@
+package cc.disruptor.test;
+
+/**
+ * Created by cl on 2018/4/25.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test.main");
+        System.out.println("args = [" + args + "]");
+    }
+}
